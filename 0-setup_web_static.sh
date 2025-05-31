dc64285ca3405104fc62f780d14b749c3c343255
NK-Adeodatus <a.nkundiman@alustudent.com>
2025-05-31 16:04:02 +0200

    0-setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,30 +2,11 @@
 # sets up the web servers for the deployment of web_static
 
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create proper HTML content matching expected output
-sudo tee /data/web_static/releases/test/index.html > /dev/null << EOF
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-EOF
-
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-
-# Add Nginx configuration if not already present
-if ! sudo grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
-    sudo sed -i '/location \/ {/i\
-\	location /hbnb_static {\
-\		alias /data/web_static/current/;\
-\	}\
-' /etc/nginx/sites-available/default
-fi
-
-sudo service nginx restart
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
